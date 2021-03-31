9c1c9712d3e32e27e14e3bb8892ec49e8450ce54
Ilya Baldin <ibaldin@renci.org>
2021-03-31 18:43:58 -0400

    Lowering python temporarily until CI/CD catches up

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  python_requires=">=3.9",
+  python_requires=">=3.8",
   install_requires=requirements,
   setup_requires=requirements,
 )

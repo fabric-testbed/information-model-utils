46aa3fd87fea3a560d0b5ebbc1274c334efea00e
Xi Yang <xiyang@es.net>
2021-04-19 22:20:25 -0400

    get_topology_json

--- fimutil/netam/netam_arm.py
+++ fimutil/netam/netam_arm.py
@@ -2,6 +2,9 @@ import fim.user as f
 from fim.graph.abc_property_graph import ABCPropertyGraph
 import requests
 from requests.auth import HTTPDigestAuth
+import json
+from jsonpath_ng import jsonpath
+from jsonpath_ng.ext import parse
 
 
 class NetAmARM:
@@ -14,21 +17,21 @@ class NetAmARM:
         self.sr_pce_user = sr_pce_user
         self.sr_pce_pass = sr_pce_pass
 
-    def get_json_topology(self) -> str:
+    def get_topology_json(self) -> object:
         s = requests.Session()
         # headers = {'X-Subscribe': 'stream'}
         r = s.get(self.sr_pce_url, auth=HTTPDigestAuth(self.sr_pce_user, self.sr_pce_pass), stream=True)
         print(r.status_code)
         if r.status_code != 200:
             raise NetAmARMSrPceError(f'Failed to retrieve SR-PCE topology from URL:{self.sr_pce_url}')
-        json_topo = ''
+        json_text = ''
         for line in r.iter_lines():
             if line:
                 str_line = line.decode("utf-8")
-                json_topo += str_line
-        if json_topo.startswith('{') and json_topo.find('Cisco-IOS-XR-infra-xtc-oper:pce/topology-nodes/topology-node') == -1:
+                json_text += str_line
+        if json_text.startswith('{') and json_text.find('Cisco-IOS-XR-infra-xtc-oper:pce/topology-nodes/topology-node') == -1:
             raise NetAmARMSrPceError(f'Invalid JSON topology retrieved from SR-PCE from URL:{self.sr_pce_url}')
-        return json_topo
+        return json.load(json_text)
 
 
 class NetAmARMSrPceError(Exception):

5a01cb8e134a809cbf00aeec5aa953274c14995f
Ilya Baldin <ibaldin@renci.org>
2021-10-15 15:37:48 -0400

    Updating to recognize new NVMe drives, however they will for now be reported as P4510

--- fimutil/ralph/nvme.py
+++ fimutil/ralph/nvme.py
@@ -1,6 +1,3 @@
-import re
-import logging
-
 from fimutil.ralph.asset import RalphAsset, RalphAssetType, RalphAssetMimatch
 
 from fimutil.ralph.ralph_uri import RalphURI
@@ -14,8 +11,8 @@ class NVMeDrive(RalphAsset):
     # "Description": "Dell Express Flash NVMe P4510 1TB SFF in PCIe
     # SSD Slot 22 in Bay 2 (0000:21:00.0)", "BDF": "0000:21:00.0"
     REGEX_FIELDS = {'BDF': ["Description", ".+\\(([0-9a-f:.]+)\\).*"],
-                    'Model': ["Description", ".+NVMe ([\\w]+).*"],
-                    'Disk': ["Description", ".+ ([\\d]+[MGTP]B) .*"]}
+                    'Model': ["Description", ".+(NVMe|CD5) ([\\w]+).*"],
+                    'Disk': ["Description", ".+ ([\\d]+[MGTP]B|[\\d]+[MGTP]) .*"]}
 
     def __init__(self, *, uri: str, ralph: RalphURI):
         super().__init__(uri=uri, ralph=ralph)
@@ -23,6 +20,9 @@ class NVMeDrive(RalphAsset):
 
     def parse(self):
         super().parse()
-        if 'NVMe' not in self.fields['Description']:
+        if 'NVMe' not in self.fields['Description'] and 'CD5' not in self.fields['Description']:
             raise RalphAssetMimatch('This is not an NVMe drive')
+        if self.fields['Model'] == 'CD5':
+            # FIXME: temporary override to old model
+            self.fields['Model'] = "P4510"
 

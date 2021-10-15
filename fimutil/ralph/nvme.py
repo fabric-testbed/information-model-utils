40f15bc2c98f235c518b681b94b4c700cd58c3e4
Ilya Baldin <ibaldin@renci.org>
2021-10-15 16:08:57 -0400

    Fixing model of NVMe parsing

--- fimutil/ralph/nvme.py
+++ fimutil/ralph/nvme.py
@@ -11,7 +11,7 @@ class NVMeDrive(RalphAsset):
     # "Description": "Dell Express Flash NVMe P4510 1TB SFF in PCIe
     # SSD Slot 22 in Bay 2 (0000:21:00.0)", "BDF": "0000:21:00.0"
     REGEX_FIELDS = {'BDF': ["Description", ".+\\(([0-9a-f:.]+)\\).*"],
-                    'Model': ["Description", ".+(NVMe|CD5) ([\\w]+).*"],
+                    'Model': ["Description", ".+(P4510|CD5) ([\\w]+).*"],
                     'Disk': ["Description", ".+ ([\\d]+[MGTP]B|[\\d]+[MGTP]) .*"]}
 
     def __init__(self, *, uri: str, ralph: RalphURI):

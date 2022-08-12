c952a5a8830763b46d5be027226a0e50e86dd3d0
Ilya Baldin <ibaldin@renci.org>
2022-08-12 15:21:02 -0400

    Small modifications to flavors to reduce their qty. Updated dependencies badge in README

--- utilities/generate_instance_flavors.py
+++ utilities/generate_instance_flavors.py
@@ -11,9 +11,34 @@ import json
 # steps/increments of what is possible
 # Disk and Ram are counted in GB
 
+"""
+# 2000 flavors
 CPUs = [x for x in range(4, 66, 2)]
 Disk = [10, 100, 500, 2000]
 RAM = [4, 8, 16, 24, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384]
+
+SPECIAL_FLAVORS = [
+    [1, 2, 10],
+    [1, 4, 10],
+    [1, 4, 100],
+    [1, 4, 500],
+    [1, 4, 2000],
+    [2, 2, 10],
+    [2, 2, 100],
+    [2, 4, 10],
+    [2, 4, 100],
+    [2, 8, 10],
+    [2, 8, 100],
+    [2, 8, 500],
+    [2, 8, 2000],
+    [64, 384, 4000]
+]
+"""
+
+# 1000 or 500 flavors
+CPUs = [x for x in range(4, 64, 4)]
+Disk = [10, 100, 500, 2000]
+RAM = [4, 8, 16, 32, 64, 128, 256, 384]
 
 SPECIAL_FLAVORS = [
     [1, 2, 10],
@@ -44,6 +69,7 @@ def generate_csv(file, dialect, delimiter) -> int:
     """
     Output a CSV
     """
+    flavors = set()
     with open(file, 'w+', newline='') as f:
         spamwriter = csv.writer(f, delimiter=delimiter, dialect=dialect,
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -54,14 +80,22 @@ def generate_csv(file, dialect, delimiter) -> int:
             for m in RAM:
                 for d in Disk:
                     flavor_name = _get_instance_name(c, m, d)
-                    spamwriter.writerow([flavor_name, c, m, d])
-                    cnt += 1
+                    if flavor_name not in flavors:
+                        spamwriter.writerow([flavor_name, c, m, d])
+                        cnt += 1
+                        flavors.add(flavor_name)
+                    else:
+                        raise Exception(f'Flavor {flavor_name} already defined')
 
         for sf in SPECIAL_FLAVORS:
             c, m, d = sf
             flavor_name = _get_instance_name(c, m, d)
-            spamwriter.writerow([flavor_name, c, m, d])
-            cnt += 1
+            if flavor_name not in flavors:
+                spamwriter.writerow([flavor_name, c, m, d])
+                cnt += 1
+                flavors.add(flavor_name)
+            else:
+                raise Exception(f'Flavor {flavor_name} already defined')
 
     return cnt
 
@@ -71,21 +105,28 @@ def generate_json(file) -> int:
     Output a JSON file in a format consumable by FIM
     """
     obj = dict()
+    flavors = set()
     # all possible combinations
     cnt = 0
     for c in CPUs:
         for m in RAM:
             for d in Disk:
                 flavor_name = _get_instance_name(c, m, d)
-                obj[flavor_name] = {"core": c, "ram": m, "disk": d}
-                cnt += 1
-
+                if flavor_name not in flavors:
+                    obj[flavor_name] = {"core": c, "ram": m, "disk": d}
+                    cnt += 1
+                    flavors.add(flavor_name)
+                else:
+                    raise Exception(f'Flavor {flavor_name} already defined')
     for sf in SPECIAL_FLAVORS:
         c, m, d = sf
         flavor_name = _get_instance_name(c, m, d)
-        obj[flavor_name] = {"core": c, "ram": m, "disk": d}
-        cnt += 1
-
+        if flavor_name not in flavors:
+            obj[flavor_name] = {"core": c, "ram": m, "disk": d}
+            cnt += 1
+            flavors.add(flavor_name)
+        else:
+            raise Exception(f'Flavor {flavor_name} already defined')
     with open(file, 'w+') as f:
         json.dump(obj, f)
 
@@ -98,6 +139,7 @@ def generate_json_ansible(file, starting_id=10) -> int:
     """
     flavors = dict()
     obj = {'flavors': flavors}
+    flavorset = set()
 
     # all possible combinations
     cnt = 0
@@ -106,19 +148,26 @@ def generate_json_ansible(file, starting_id=10) -> int:
         for m in RAM:
             for d in Disk:
                 flavor_name = _get_instance_name(c, m, d)
-                flavor_key = 'flavor' + str(flavor_id)
-                flavors[flavor_key] = {"vcpu": c, "ram": m*1024, "disk": d, "name": flavor_name, "id": flavor_id}
-                cnt += 1
-                flavor_id += 1
+                if flavor_name not in flavorset:
+                    flavor_key = 'flavor' + str(flavor_id)
+                    flavors[flavor_key] = {"vcpu": c, "ram": m*1024, "disk": d, "name": flavor_name, "id": flavor_id}
+                    cnt += 1
+                    flavor_id += 1
+                    flavorset.add(flavor_name)
+                else:
+                    raise Exception(f'Flavor {flavor_name} already defined')
 
     for sf in SPECIAL_FLAVORS:
         c, m, d = sf
         flavor_name = _get_instance_name(c, m, d)
-        flavor_key = 'flavor' + str(flavor_id)
-        flavors[flavor_key] = {"vcpu": c, "ram": m*1024, "disk": d, "name": flavor_name, "id": flavor_id}
-        cnt += 1
-        flavor_id += 1
-
+        if flavor_name not in flavorset:
+            flavor_key = 'flavor' + str(flavor_id)
+            flavors[flavor_key] = {"vcpu": c, "ram": m*1024, "disk": d, "name": flavor_name, "id": flavor_id}
+            cnt += 1
+            flavor_id += 1
+            flavorset.add(flavor_name)
+        else:
+            raise Exception(f'Flavor {flavor_name} already defined')
     with open(file, 'w+') as f:
         json.dump(obj, f)
 

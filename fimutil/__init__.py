747ccbeaaa3c74d8ebe64258cbf6be523982603e
Ilya Baldin <ibaldin@renci.org>
2023-06-02 18:07:44 -0400

    Upping release

--- fimutil/__init__.py
+++ fimutil/__init__.py
@@ -3,5 +3,5 @@
 This is a package of Information Model utilitied for FABRIC
 for scanning different types of sites
 """
-__VERSION__ = "1.5.0rc1"
+__VERSION__ = "1.5.0rc3"
 __version__ = __VERSION__

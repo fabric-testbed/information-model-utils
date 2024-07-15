07e6de6cc93c816c7046d332b05cb58f6bd697dd
Hussamuddin Nasir <nasir@netlab.uky.edu>
2024-07-15 16:30:53 -0400

    Change search query for hostnames to look for hostanames starting with <SITE>-w Hope is that we dont have sites named "something-w" This would resolve the issue of searches using EDUKY-w and UKY-w which would produce incorrect results when a regex was used.
    Additionally i noticed that the query was done twice since only 10
    records were being fetched at a time. This was due to a default param of
    limit=10 being added by the underlying query API. I fixed this put
    intentionally adding a param limit=100.

--- fimutil/ralph/site.py
+++ fimutil/ralph/site.py
@@ -102,7 +102,8 @@ class Site:
             except ValueError:
                 logging.warning('Unable to find PTP server in site, continuing')
 
-        query = {'hostname__regex': f'{self.name.lower()}-w[0123456789]' + self.domain}
+        #query = {'hostname__regex': f'{self.name.lower()}-w[0123456789]' + self.domain}
+        query = {'hostname__startswith': f'{self.name.lower()}-w','limit': 100}
         results = self.ralph.get_json_object(self.ralph.base_uri + 'data-center-assets/?' +
                                              urlencode(query))
 
@@ -112,11 +113,11 @@ class Site:
         # not sure why the regex doesn't work as expected, ({self.name.lower()}-w[0123456789]+)
         # so instead another simple regex to look for workers with two digit indexes
 
-        query = {'hostname__regex': f'{self.name.lower()}-w[0123456789][0123456789]' + self.domain}
-        results = self.ralph.get_json_object(self.ralph.base_uri + 'data-center-assets/?' +
-                                             urlencode(query))
+        #query = {'hostname__regex': f'{self.name.lower()}-w[0123456789][0123456789]' + self.domain}
+        #results = self.ralph.get_json_object(self.ralph.base_uri + 'data-center-assets/?' +
+        #                                     urlencode(query))
 
-        worker_urls.extend(pyjq.one('[ .results[].url ]', results))
+        #worker_urls.extend(pyjq.one('[ .results[].url ]', results))
 
         logging.info(f'Identified {len(worker_urls)} workers')
 
